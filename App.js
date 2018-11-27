2559c0f8923a63b5dfce37935ccf603aca566e0d
hasitha <hasitha@empite.com>
2018-11-27 15:37:45 +0530

    convert into jsx

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
-const Pet = (props) => {
-    return React.createElement("div",{},
-        React.createElement("h1",{},props.name),
-        React.createElement("h1",{},props.animal),
-        React.createElement("h1",{},props.breed)
-    )
-} 
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
+class App extends React.Component {
+  TitleClick() {
+    alert("Click Adopt Me!");
+  }
 
-class App extends React.Component 
-{
-    TitleClick() {
-        alert('Click Adopt Me!');
-    }
-
-    render() {
-        return React.createElement("div",{}, [
-            React.createElement("h1",{ onClick: this.TitleClick },'Adopt Me!'),
-            React.createElement(Pet, {
-                name: 'Dongi',
-                animal: 'Dog',
-                breed: 'Havanese'
-            }),
-            React.createElement(Pet, {
-                name: 'Cati',
-                animal: 'Cat',
-                breed: 'Mixed'
-            })
-        ])
-    }
+  render() {
+    return (
+      <div>
+        <h1 onClick={this.TitleClick}>Adopt Me!</h1>
+        <Pet name="Dongi" animal="dog" breed="Havanese" />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(React.createElement(App),document.getElementById('root'));
+render(React.createElement(App), document.getElementById("root"));
---
+++ Pet.js
@@ -0,0 +1,10 @@
+import React from "react";
+export const Pet = props => {
+  return (
+    <div className="lol" id="my-id">
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed} </h1>
+    </div>
+  );
+};
